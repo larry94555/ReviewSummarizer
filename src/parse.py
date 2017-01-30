74b2bf0af0aa6e16d0fec0c6ffebb0055980f156
larry94555 <larry.freeman@gmail.com>
2017-01-29 20:18:25 -0800

    Feature: Adding parsing of json
    Changed code from json.load to json.loads.

--- src/parse.py
+++ src/parse.py
@@ -1,8 +1,24 @@
 import json
+import time
 
 json_file = "/mnt/c/machine-learning/ReviewSummarizer/data/yelp_academic_dataset_review.json"
 
-json_data = open(json_file)
+def time_passed(start_time):
+    return time.time() - start_time
 
-data = json.load(json_data)
+num=0
+start_time = time.time()
+with open(json_file) as f:
+    for line in f:
+        data = json.loads(line)
+        #reviews = pd.DataFrame(list(data))
+        num += 1
+        if num % 100000 == 0:
+            print "num = ",num,time_passed(start_time)
+            #print len(reviews)
+            #print data['text']
+
+print "duration:",time_passed(start_time)
+
+
 
